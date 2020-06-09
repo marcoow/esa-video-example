0ca4f89661e3e3d3aec3ee025145286af96341a1
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-06-09 12:44:01 +0200

    add authorization

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,19 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   namespace = 'api';
+
+  @service session;
+
+  @computed('session.data.authenticated.token')
+  get headers() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      headers['token'] = this.session.data.authenticated.token;
+    }
+
+    return headers;
+  }
 }
--- server/mocks/messages.js
+++ server/mocks/messages.js
@@ -6,30 +6,35 @@ module.exports = function(app) {
   let messagesRouter = express.Router();
 
   messagesRouter.get('/', function(req, res) {
-    res.send({
-      'data': [
-        {
-          'type': 'messages',
-          'id': '1',
-          'attributes': {
-            'sender': 'Tomster',
-            'subject': "Hey Zoey 👋",
-            'body': "How is it going? Will I see you at EmberConf next year?",
-            'sent-at': moment().toJSON()
+    let { token } = req.headers;
+    if (token === 'secret token!') {
+      res.send({
+        'data': [
+          {
+            'type': 'messages',
+            'id': '1',
+            'attributes': {
+              'sender': 'Tomster',
+              'subject': "Hey Zoey 👋",
+              'body': "How is it going? Will I see you at EmberConf next year?",
+              'sent-at': moment().toJSON()
+            }
+          },
+          {
+            'type': 'messages',
+            'id': '2',
+            'attributes': {
+              'sender': 'EmberConf',
+              'subject': 'Registration Confirmation for EmberConf 2021',
+              'body': 'Thanks so much for registering to join us at EmberConf! You do NOT need to print this confirmation, but photo ID may be required for entry.',
+              'sent-at': moment().subtract(3, 'days').toJSON()
+            }
           }
-        },
-        {
-          'type': 'messages',
-          'id': '2',
-          'attributes': {
-            'sender': 'EmberConf',
-            'subject': 'Registration Confirmation for EmberConf 2021',
-            'body': 'Thanks so much for registering to join us at EmberConf! You do NOT need to print this confirmation, but photo ID may be required for entry.',
-            'sent-at': moment().subtract(3, 'days').toJSON()
-          }
-        }
-      ]
-    });
+        ]
+      });
+    } else {
+      res.status(401).end();
+    }
   });
 
   app.use('/api/messages', messagesRouter);
