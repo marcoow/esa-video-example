df6b873861b50db56942a5758de453710898b5e6
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-06-09 12:12:43 +0200

    require authentication for inbox

--- app/routes/inbox.js
+++ app/routes/inbox.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class InboxRoute extends Route {
+  @service session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+
   model() {
     return this.store.findAll('message');
   }
